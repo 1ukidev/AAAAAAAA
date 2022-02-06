6dc87daf0a9ffc33e7071776a658c3aff76cb99c
1ukidev <leo.monteiro06@live.com>
2022-02-06 14:05:31 -0300

    AAAAAAAA

---
+++ AAAAAAA.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace AAAAAAAA
+{
+    static class AAAAAAA
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AAAAAAAA());
+        }
+    }
+}
---
+++ AAAAAAAA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AAAAAAAA
+{
+    public partial class AAAAAAAA : Form
+    {
+        public AAAAAAAA()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("AAAAAAAAAAAAA", "AAAAAAAAAAAAA");
+        }
+    }
+}
